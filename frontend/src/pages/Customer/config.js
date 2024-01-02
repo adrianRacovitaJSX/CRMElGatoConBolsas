70fbc63e593c55c25df9edc48550cdfb0af9b013
adrianracovitaliviu <adrianliviuracovita@gmail.com>
2024-01-02 21:26:19 +0100

    1.0

---
+++ frontend/src/pages/Customer/config.js
@@ -0,0 +1,29 @@
+export const fields = {
+  type: {
+    type: 'selectwithfeedback',
+    renderAsTag: true,
+    options: [
+      { value: 'people', label: 'people', color: 'magenta' },
+      { value: 'company', label: 'company', color: 'blue' },
+    ],
+    required: true,
+    hasFeedback: true,
+  },
+  name: {
+    type: 'string',
+    disableForForm: true,
+  },
+  country: {
+    type: 'country',
+    // color: 'red',
+    disableForForm: true,
+  },
+  phone: {
+    type: 'phone',
+    disableForForm: true,
+  },
+  email: {
+    type: 'email',
+    disableForForm: true,
+  },
+};
